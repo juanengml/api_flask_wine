68a25438c6c60c24efdefd989a37164d18dc4544
juanengml <juanengml@gmail.com>
2019-12-08 16:40:30 -0300

    Update API wine

---
+++ api.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, redirect, url_for, flash, jsonify
+import numpy as np
+import pickle as p
+import json
+
+
+app = Flask(__name__)
+
+
+@app.route('/api/', methods=['POST'])
+def API():
+    data = request.get_json()
+    predicao = np.array2string(modelo.predict(data))
+    return jsonify(predicao)
+
+if __name__ == '__main__':
+    modelfile = 'modelo_vinho.pickle'
+    modelo = p.load(open(modelfile, 'rb'))
+    app.run(debug=True, host='0.0.0.0')
