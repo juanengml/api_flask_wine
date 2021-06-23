41ccf2eaff622c5f440166a965b874e3a5e9ff64
Sr Marinho <37448831+juanengml@users.noreply.github.com>
2021-06-23 16:20:05 -0300

    Update api.py

--- api.py
+++ api.py
@@ -1,28 +1,14 @@
-from flask import Flask, request, redirect, url_for, flash, jsonify
-import numpy as np
-import pickle as p
-import json
-from datetime import datetime as dt
+from flask import Flask
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
+api = Api(app)
 
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
 
-@app.route('/api/', methods=['POST'])
-def API():
-    data = request.get_json()
-    predicao = np.array2string(modelo.predict(data))
-    return jsonify(predicao)
-@app.route("/")
-def home():
-    return """
-    <center>
-      <h1> API WINE ALIVE !!!</h1>
-      <br>
-      <h2>{}</h2>
-    </center>
-    """.format(dt.now())
+api.add_resource(HelloWorld, '/')
 
 if __name__ == '__main__':
-    modelfile = 'modelo_vinho.pickle'
-    modelo = p.load(open(modelfile, 'rb'))
-    app.run(debug=True, host='0.0.0.0',port=8080)
+    app.run(host="0.0.0.0",port=8080,debug=True)
