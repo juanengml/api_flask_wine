57069cf0bcdae8d9e70c21fc83428ecba3a81f86
Sr Marinho <37448831+juanengml@users.noreply.github.com>
2021-06-23 15:49:09 -0300

    Update api.py

--- api.py
+++ api.py
@@ -2,7 +2,7 @@ from flask import Flask, request, redirect, url_for, flash, jsonify
 import numpy as np
 import pickle as p
 import json
-
+from datetime import datetime as dt
 
 app = Flask(__name__)
 
@@ -12,6 +12,15 @@ def API():
     data = request.get_json()
     predicao = np.array2string(modelo.predict(data))
     return jsonify(predicao)
+@app.route("/")
+def home():
+    return """
+    <center>
+      <h1> API WINE ALIVE !!!</h1>
+      <br>
+      <h2>{}</h2>
+    </center>
+    """.format(dt.now())
 
 if __name__ == '__main__':
     modelfile = 'modelo_vinho.pickle'
