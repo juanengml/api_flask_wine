c0ebe4f4eed56c4fec6bcfc3f691d304563b54f3
Sr Marinho <37448831+juanengml@users.noreply.github.com>
2021-06-24 13:44:29 -0300

    Update api.py

--- api.py
+++ api.py
@@ -1,12 +1,13 @@
 from flask import Flask
 from flask_restful import Resource, Api
+from datetime import datetime as dt
 
 app = Flask(__name__)
 api = Api(app)
 
 class HelloWorld(Resource):
     def get(self):
-        return {'Data': 'Flavor'}
+        return {'Data': 'Flavor',"datetime":str(dt.now())}
 
 api.add_resource(HelloWorld, '/')
 
